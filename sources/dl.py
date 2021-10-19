8106fdcce7bb91e716006949eb206e96040870aa
Rudloff <valentin.rudloff@navya.tech>
2021-10-19 19:01:42 +0200

    create a download function

--- sources/dl.py
+++ sources/dl.py
@@ -7,48 +7,47 @@ import getpass, imaplib
 import os
 import sys
 
-detach_dir = '.'
-if 'attachments' not in os.listdir(detach_dir):
-    os.mkdir('attachments')
-
-userName = "birdeyecergy@gmail.com"
-passwd = "eijjnyeceafqzkni"
-
-try:
-    imapSession = imaplib.IMAP4_SSL('imap.gmail.com')
-    typ, accountDetails = imapSession.login(userName, passwd)
-    if typ != 'OK':
-        print('Not able to sign in!')
-        raise
-    
-    imapSession.select("INBOX")
-    typ, data = imapSession.search(None, 'ALL')
-    if typ != 'OK':
-        print('Error searching Inbox.0')
-        raise
-    
-    for num in data[0].split():
-        typ, data = imapSession.fetch(num, '(RFC822)' )
-        raw_email = data[0][1]
-        # converts byte literal to string removing b''
-        raw_email_string = raw_email.decode('utf-8')
-        email_message = email.message_from_string(raw_email_string)
-        # downloading attachments
-        for part in email_message.walk():
-            # this part comes from the snipped I don't understand yet... 
-            if part.get_content_maintype() == 'multipart':
-                continue
-            if part.get('Content-Disposition') is None:
-                continue
-            fileName = part.get_filename()
-            if bool(fileName):
-                filePath = os.path.join('.', fileName)
-                if not os.path.isfile(filePath) :
-                    fp = open(filePath, 'wb')
-                    fp.write(part.get_payload(decode=True))
-                    fp.close()
-                subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
-    imapSession.close()
-    imapSession.logout()
-except :
-    print('Not able to download all attachments.')
+def download_images(user_name=None, password=None):
+    """
+    Download the images using Gmail
+    @param user_name: User name of the Gmail account
+    @param password: password of the Gmail account
+    """
+    if not user_name or not password:
+        raise "no credential provided"
+    try:
+        imapSession = imaplib.IMAP4_SSL('imap.gmail.com')
+        typ, accountDetails = imapSession.login(user_name, password)
+        if typ != 'OK':
+            raise 'Not able to sign in!'
+        
+        imapSession.select("INBOX")
+        typ, data = imapSession.search(None, 'ALL')
+        if typ != 'OK':
+            raise 'Error searching Inbox.0'
+        
+        filenames = []
+        for num in data[0].split():
+            typ, data = imapSession.fetch(num, '(RFC822)' )
+            raw_email = data[0][1]
+            # converts byte literal to string removing b''
+            raw_email_string = raw_email.decode('utf-8')
+            email_message = email.message_from_string(raw_email_string)
+            # downloading attachments
+            for part in email_message.walk():
+                # this part comes from the snipped I don't understand yet... 
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
+                fileName = part.get_filename()
+                if bool(fileName):
+                    filenames.append(part.get_payload(decode=True))
+        imapSession.close()
+        imapSession.logout()
+    except :
+        print('Not able to download all attachments.')
+    if filenames == []:
+        return [None]
+    else:
+        return filenames
