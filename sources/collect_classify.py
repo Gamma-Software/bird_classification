1af137c4fdd6b66890f0f6fc310e4f96a9b785c5
Rudloff <valentin.rudloff@navya.tech>
2021-10-28 14:42:25 +0200

    clean main function

--- sources/collect_classify.py
+++ sources/collect_classify.py
@@ -1,34 +1,16 @@
 import sys
 import time
-import retrieve_image_mail
-import detection_classification as dc
-import tensorflow.compat.v2 as tf
-from PIL import Image
+from retrieve_image_mail import EmailParser
 import io
-import cv2 
-
-import matplotlib.pyplot as plt
-
-from PIL import Image
-from PIL import ImageColor
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageOps
-
 import numpy as np
+
 
 def credential():
         f = open("data/credential.txt")
-        user = f.readline()
-        passwd = f.readline()
+        user = f.readline().strip("\n")
+        passwd = f.readline().strip("\n")
         f.close()
         return user, passwd
-
-
-def display_image(image):
-    fig = plt.figure(figsize=(20,15))
-    plt.grid(False)
-    plt.imshow(image)
 
 
 # cherrypick from https://github.com/cmoon4/backyard_birdbot/blob/main/bird_detect.py
@@ -38,15 +20,6 @@ if __name__ == "__main__":
         #images = retrieve_image_mail.download_images(user_name, password)
         #[print(dc.detect_bird(image)) for image in images]
         #image = Image.open("tests/data/01.jpg")
-        src = cv2.imread("tests/data/04.jpg")
-        image = cv2.cvtColor(src, cv2.COLOR_BGR2RGB )
-        image_converted = tf.image.convert_image_dtype(image, tf.float32)[tf.newaxis, ...]
-        result, res = dc.detect_bird(image_converted)
-        result_plt={key:value.numpy() for key,value in result.items()}
-        num_bird=np.size(res["names"])
-        box_indices=tf.zeros(shape=(num_bird,),dtype=tf.int32)  
-        cropped_img=tf.image.crop_and_resize(image_converted,res["boxes"],box_indices,[224,224])
-        display_image(cropped_img)
 
         sys.exit(0)
         while True:
