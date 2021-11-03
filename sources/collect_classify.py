daa29fe48d6405ce7e476ba4b64476a40845d5ee
valentin.rudloff <valentin.rudloff.perso@gmail.com>
2021-11-03 23:04:29 +0100

    update algo to append the last and new data

--- sources/collect_classify.py
+++ sources/collect_classify.py
@@ -16,14 +16,18 @@ def credential():
         f.close()
         return user, passwd
 
-
 # cherrypick from https://github.com/cmoon4/backyard_birdbot/blob/main/bird_detect.py
 if __name__ == "__main__":
     user_name, password = credential()
     email_parser = EmailParser(user_name, password)
+    database_file = 'sqlite:////mnt/data/workspace/bird_classification/data/database.db'
     try:
-        engine = create_engine('sqlite:////mnt/data/workspace/bird_classification/data/database.db', echo=False)
-        email_parser.get_todays_email().to_sql('dates', con=engine, if_exists='append')
+        engine = create_engine(database_file, echo=False)
+        last_data = pd.read_sql('dates', database_file)
+        new_data = email_parser.get_todays_email()
         #email_parser.get_all_email().to_sql('dates', con=engine, if_exists='append')
+        data_appened = np.append(np.array(last_data.iloc[:, 1]), np.array(new_data.iloc[:]))
+        to_pandas = pd.DataFrame(data_appened).rename(columns={0: "dates"}).sort_values("dates", ascending=False).reset_index(drop=True)
+        to_pandas.to_sql('dates', con=engine, if_exists='replace')
     except KeyboardInterrupt:
         sys.exit(0)
